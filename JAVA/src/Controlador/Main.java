94a88b1955a228b78e0033c89e1f6beb3b29c4a2
Claudio-UTP <u22216115@utp.edu.pe>
2024-10-27 11:29:47 -0500

    Importe del proyecto

---
+++ JAVA/src/Controlador/Main.java
@@ -0,0 +1,12 @@
+package Controlador;
+
+import Vista.frmLogin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        frmLogin iniciar = new frmLogin();
+        iniciar.setVisible(true);
+    }
+    
+}
